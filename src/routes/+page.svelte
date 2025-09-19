3129b4984e5e1b1744ab92f625e916baa0c2821c
frankfium <thebrewcrewfrank@gmail.com>
2025-09-19 16:03:14 -0400

    fix: mobile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,11 +2,9 @@
   import '../styles/styles.css';
   import About from '../components/About.svelte';
   import Head from '../components/Head.svelte';
-  import Mobile from '../components/Mobile.svelte';
 </script>
 
 <Head />
 <section>
   <About />
-  <Mobile />
 </section>
